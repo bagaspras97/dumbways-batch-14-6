002ffe3ef756bfee5010a9d91ecb80c3fb02801a
bagaspras97 <59312912+bagaspras97@users.noreply.github.com>
2020-01-25 22:30:04 +0700

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,44 +1,19 @@
 function minMax(arr){
-    var newArr = [];
-    
-    
-    if(arr[0]=='a'){
-      newArr.push(arr[0]);
-      arr.reverse();
-      newArr.push(arr[0]);
-      console.log(newArr);
-    } else{
-      arr.shift();
-      
-      if(arr[0]=='a'){
-        newArr.push(arr[0]);
-        arr.reverse();
-        newArr.push(arr[0]);
-        console.log(newArr);
-      }else{
-        arr.shift();
-        
-        if(arr[0]=='a'){
-          newArr.push(arr[0]);
-          arr.reverse();
-          newArr.push(arr[0]);
-          console.log(newArr)
-      }else{
-        arr.shift();
-        console.log(arr)
-      }
-    } 
-      
-    }
-    }
-    
-    var data = ['h','g','a','b','d','f']; 
-    var dataLain = ['a','b','c','d']; 
-    minMax(data);
-    minMax(dataLain);
-    
-     
-    
-    
-    
-    
+var newArr = [];
+
+for(i=0; i<arr.length; i++){
+if(arr[0]=='a'){
+  newArr.push(arr[0]);
+  arr.reverse();
+  newArr.push(arr[0]);
+  console.log(newArr);
+} else{
+  arr.shift();
+}
+}
+}
+
+var data = ['h','g','a','b','d','f']; 
+var dataLain = ['a','b','c','d']; 
+minMax(data);
+minMax(dataLain);
