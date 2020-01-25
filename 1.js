7843def204ab41fa22786ed0e32c84e23e3e762a
bagaspras97 <59312912+bagaspras97@users.noreply.github.com>
2020-01-25 20:53:25 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,14 @@
+var jmlBurung = 6969;
+var burungMati = 111/1000;
+var wktLahir = 21;
+var jmlHari = 441;
+
+for (i=0; i<wktLahir; i++){
+  if(wktLahir<=jmlHari){
+    wktLahir += 21;
+    var a = jmlBurung - jmlBurung*burungMati;
+    jmlBurung = a+a;
+}
+ 
+}
+console.log(jmlBurung); 
